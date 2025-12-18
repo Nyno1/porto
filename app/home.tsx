97ef9f48d7035e13bc74412d2122adf00ed30ac7
Nyno1 <nynxd123@gmail.com>
2025-12-18 07:58:58 +0700

    tambahan

--- app/home.tsx
+++ app/home.tsx
@@ -4,7 +4,7 @@ import { ShootingStars } from "@/components/ui/shooting-stars";
 import { StarsBackground } from "@/components/ui/stars-background";
 export function ShootingStarsAndStarsBackgroundDemo() {
   return (
-    <div className="h-[40rem] rounded-md flex flex-col items-center justify-center relative w-full">
+    <div className="h-[55rem] rounded-md flex flex-col items-center justify-center relative w-full">
       <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
         <span>Nashat Akram</span>
       </h2>
